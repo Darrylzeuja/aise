3098fb89a5bbcf0301253c7dc8b1230a22db539c
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2019-01-16 15:38:52 +0100

    Fix argv

--- Cours_3/pipe.c
+++ Cours_3/pipe.c
@@ -5,41 +5,36 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int argc, char ** argv )
-{
+int main(int argc, char ** argv ) {
 	int pp[2];
 	pipe(pp);
 
 	pid_t child1 = fork();
 
-	if( child1 == 0)
-	{
+	if( child1 == 0) {
 		/* Replace stdout with the write end of the pipe */
 		dup2(pp[1], STDOUT_FILENO);
 		/* Close read end of the pipe */
 		close(pp[0]);
 		close(pp[1]);
 		/* Run command */
-		char * argv[] = {"printf","Salut Tout Le Monde ", NULL};
-		execvp( argv[0], argv);
+		char * argv1[] = {"printf","Salut Tout Le Monde ", NULL};
+		execvp( argv1[0], argv);
 	}
-	else
-	{
+	else {
 		pid_t child2 = fork();
 	
-		if(child2 == 0)
-		{
+		if(child2 == 0) {
 			/* Replace stdin with the read end of the pipe */
 			dup2(pp[0], STDIN_FILENO);
 			/* Close write end of the pipe */
 			close(pp[1]);
 			close(pp[0]);
 			/* Run command */
-			char * argv[] = {"wc","-c", " ", NULL};
-			execvp( argv[0], argv);
+			char * argv2[] = {"wc","-c", " ", NULL};
+			execvp( argv2[0], argv);
 		}
-		else
-		{
+		else {
 			/* Close both end of the pipe */
 			close(pp[0]);
 			close(pp[1]);
