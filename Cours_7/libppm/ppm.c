f429685707b567d1306101f9a80801c458e59004
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2019-02-14 05:21:44 +0100

    PPM skeleton

---
+++ Cours_7/libppm/ppm.c
@@ -0,0 +1,24 @@
+#include "ppm.h"
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+ 
+int ppm_image_init( struct ppm_image *im , int w , int h )
+{
+ 
+    return 0;
+}
+ 
+int ppm_image_release( struct ppm_image *im )
+{
+ 
+    return 0;
+}
+ 
+int ppm_image_dump( struct ppm_image *im, char * path )
+{
+ 
+    return 0;
+}
